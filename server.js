56c7dc1f1af5f05ba3ae98ddc43a736ce30be97c
RishabBaisakh <rishab.baisakh@gmail.com>
2025-07-24 15:54:45 -0400

    bug fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const { connectDB } = require("./config/database");
+const cors = require("cors");
 const routes = require("./routes");
 
 const port = process.env.PORT || 3000;
