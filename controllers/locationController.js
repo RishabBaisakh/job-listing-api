83b7f046ced69158c7efa954d1a0fdfb82e1289f
RishabBaisakh <rishab.baisakh@gmail.com>
2025-07-24 07:57:09 -0400

    updated locations controller

---
+++ controllers/locationController.js
@@ -0,0 +1,15 @@
+const Location = require("../models/location");
+
+const getAllLocations = async (req, res) => {
+  try {
+    const locations = await Location.find();
+    return res.status(200).json(locations);
+  } catch (error) {
+    console.error("Error fetching locations:", error);
+    return res.status(500).json({ message: "Failed to fetch locations" });
+  }
+};
+
+module.exports = {
+  getAllLocations,
+};
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const jobRoutes = require("./jobRoutes");
+const locationRoutes = require("./locationRoutes");
 
 router.use("/jobs", jobRoutes);
+router.use("/locations", locationRoutes);
 
 module.exports = router;
