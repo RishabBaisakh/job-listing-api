14fb6373c81d107a3f04e366b0b271cccae6d639
RishabBaisakh <rishab.baisakh@gmail.com>
2025-07-24 16:14:16 -0400

    cors error fix

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-  })
-);
+app.use(cors());
 
 // middlewares
 app.use(express.json());
